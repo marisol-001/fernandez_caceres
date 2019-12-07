258167756bef3c7f58fd3d8de00307c28571a8f8
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-07 10:44:21 -0500

    se ingreso ejercicio3

---
+++ ejercicio3.py
@@ -0,0 +1,13 @@
+#ejercicio3
+import os
+#declarar
+tiempodesalida,tiempodellegada,tiempo_total=0,0,0
+
+tiempodesalida=int(os.sys.argv[1])
+tiempodellegada=int(os.sys.argv[2])
+tiempo_total=tiempodesalida+tiempodellegada
+
+#procesing
+if(tiempo_total>50):
+    print(tiempo_total,"velocidad excelente")
+#fin-if
