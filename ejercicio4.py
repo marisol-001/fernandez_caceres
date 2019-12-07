5124cc274b58c02348e12eba7079c7e06fcde7b6
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-07 16:32:22 -0500

    se ingreso ejercicio4

---
+++ ejercicio4.py
@@ -0,0 +1,14 @@
+#ejercicio4
+import os
+#declarar las variables
+talla1,talla2,tallafinal=0,0,0
+
+talla1=int(os.sys.argv[1])
+talla2=int(os.sys.argv[2])
+
+tallafinal=talla2+talla1
+#preocesing
+if(tallafinal<60):
+    print("tratamiento bueno")
+#fin-if
+
