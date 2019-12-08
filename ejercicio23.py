f7853b1fb1c6a39f623450f1e51514801a3ef6cd
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-07 23:49:39 -0500

    se ingreso ejercicio23

---
+++ ejercicio23.py
@@ -0,0 +1,17 @@
+#ejercicio 23 doble
+import os
+#declarar
+lado1,lado2,area=0.0,0.0,0.0
+#Input via os
+lado1=float(os.sys.argv[1])
+lado2=float(os.sys.argv[2])
+
+area=lado1*lado2
+
+#processing
+if(area>60):
+    print("cuadrado grande")
+else:
+    print("cuadrado pequeño")
+
+#fin_if
