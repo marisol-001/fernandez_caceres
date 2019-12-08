ac757b411c5445c4a35733b2598c30f5cee45e3e
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-07 23:46:56 -0500

    se ingreso ejercicio21

---
+++ ejercicio21.py
@@ -0,0 +1,22 @@
+#EJERCICIO 21 doble
+import os
+#declaracion
+nota1=0.0
+nota2=0.0
+nota3=0.0
+nota4=0.0
+
+#Input
+
+nota1=float(os.sys.argv[1])
+nota2=float(os.sys.argv[2])
+nota3=float(os.sys.argv[3])
+nota4=float(os.sys.argv[4])
+
+#procesing
+promedio=(nota1+nota2+nota3+nota4)/4
+if(promedio>10.5):
+    print("curso aprobado")
+else:
+    print("curso desaprobado")
+#fin_if
