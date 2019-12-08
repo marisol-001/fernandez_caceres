1ecacee420ab291273b6e4dc049f8fe0bf8cac8a
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-07 22:24:12 -0500

    se ingreso ejercicio7 se ingreso ejercicio8

---
+++ ejercicio7.py
@@ -0,0 +1,12 @@
+#ejercicio 7
+import os
+#Declarar
+Usuario=""
+clave=""
+#Input via os
+Usuario=str(os.sys.argv[1])
+clave=str(os.sys.argv[2])
+#processing
+if (Usuario=="sol")and(clave=="1234"):
+  print("Acceso correcto")
+#fin_if
