688cf7315eca2d157ab6a50b530e9136e5e7b914
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-07 22:49:15 -0500

    se ingreso ejercicio8 se ingreso ejercicio9

---
+++ ejercicio9.py
@@ -0,0 +1,17 @@
+# ejercicio9
+import os
+#declaracion
+nombre=""
+acciones=0
+total=0
+
+#Input via os
+nombre=str(os.sys.argv[1])
+acciones=int(os.sys.argv[2])
+
+
+#procesing
+total=acciones
+if(total>10):
+    print(nombre,"socio_mayoritario")
+#fin_if
