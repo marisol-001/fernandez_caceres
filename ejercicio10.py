40cdbb54440128bc6da750c78a548444567d712b
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-07 22:52:46 -0500

    se ingreso ejercicio10

---
+++ ejercicio10.py
@@ -0,0 +1,12 @@
+#ejercicio10
+import os
+#declarar
+numerodeentradas=0
+
+#Input via os
+numerodeentradas=int(os.sys.argv[1])
+
+#procesing
+if(numerodeentradas>50):
+    print("gratis 2 entradas")
+#fin_if
