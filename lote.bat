402b11210d33142b6fe0708a55d2f2766fd14095
Marisol <dfernandezgas@unprg.edu.pe>
2019-12-10 13:15:55 -0500

    se ingreso lote.bat

---
+++ lote.bat
@@ -0,0 +1,127 @@
+python fernandez_Gastelo/ejercicio1.py  40
+python fernandez_Gastelo/ejercicio1.py  30
+python fernandez_Gastelo/ejercicio1.py  50
+
+python fernandez_Gastelo/ejercicio2.py   20 40 60
+python fernandez_Gastelo/ejercicio2.py   80 100 60
+python fernandez_Gastelo/ejercicio2.py   100 20 100
+
+python fernandez_Gastelo/ejercicio3.py   100  50
+python fernandez_Gastelo/ejercicio3.py   50   80
+python fernandez_Gastelo/ejercicio3.py   80   90
+
+python fernandez_Gastelo/ejercicio4.py   30    20
+python fernandez_Gastelo/ejercicio4.py   40    80
+python fernandez_Gastelo/ejercicio4.py   40    35
+
+python fernandez_Gastelo/ejercicio5.py   80  90
+python fernandez_Gastelo/ejercicio5.py   90  900
+python fernandez_Gastelo/ejercicio5.py   600  100
+
+python fernandez_Gastelo/ejercicio6.py   11 12 13 11
+python fernandez_Gastelo/ejercicio6.py   10 11 15 20
+python fernandez_Gastelo/ejercicio6.py   12 13 20 17
+
+python fernandez_Gastelo/ejercicio7.py   "sol"   "1234"
+python fernandez_Gastelo/ejercicio7.py   "mar"    "123"
+python fernandez_Gastelo/ejercicio7.py   "fer"    "546"
+
+python fernandez_Gastelo/ejercicio8.py   40   2
+python fernandez_Gastelo/ejercicio8.py   20   70
+python fernandez_Gastelo/ejercicio8.py   10   20
+
+python fernandez_Gastelo/ejercicio9.py     "rosa"    200
+python fernandez_Gastelo/ejercicio9.py     "juan"    150
+python fernandez_Gastelo/ejercicio9.py     "beto"    20
+
+
+python fernandez_Gastelo/ejercicio10.py     60
+python fernandez_Gastelo/ejercicio10.py     90
+python fernandez_Gastelo/ejercicio10.py     20
+
+
+python fernandez_Gastelo/doble11.py    60
+python fernandez_Gastelo/doble11.py    45
+python fernandez_Gastelo/doble11.py    20
+
+
+python fernandez_Gastelo/doble12.py    60
+python fernandez_Gastelo/doble12.py    30
+python fernandez_Gastelo/doble12.py    90
+
+
+
+python fernandez_Gastelo/doble13.py    100 20 60
+python fernandez_Gastelo/doble13.py    30  50 10
+python fernandez_Gastelo/doble13.py    60  20 200
+
+
+
+python fernandez_Gastelo/doble14.py    25 60
+python fernandez_Gastelo/doble14.py    30 70
+python fernandez_Gastelo/doble14.py    60 20
+
+python fernandez_Gastelo/doble15.py    20  28
+python fernandez_Gastelo/doble15.py    30  50
+python fernandez_Gastelo/doble15.py    60  10
+
+python fernandez_Gastelo/doble16.py    200 800
+python fernandez_Gastelo/doble16.py    300  200
+python fernandez_Gastelo/doble16.py    600  10
+
+
+python fernandez_Gastelo/doble17.py    12 11 13 11
+python fernandez_Gastelo/doble17.py    03 00 14 12
+python fernandez_Gastelo/doble17.py    10 10 12 15
+
+python fernandez_Gastelo/doble18.py    "sol"  "1234"
+python fernandez_Gastelo/doble18.py    "dhg"  "125"
+python fernandez_Gastelo/doble18.py    "ser"  "485"
+
+python fernandez_Gastelo/doble19.py    25 60
+python fernandez_Gastelo/doble19.py    33 20
+python fernandez_Gastelo/doble19.py    65 10
+
+python fernandez_Gastelo/doble20.py    28
+python fernandez_Gastelo/doble20.py    35
+python fernandez_Gastelo/doble20.py    68
+
+
+python fernandez_Gastelo/multiple21.py   200 100 50 30
+python fernandez_Gastelo/multiple21.py   30  20  10 50
+python fernandez_Gastelo/multiple21.py   10  10  20  80
+
+python fernandez_Gastelo/multiple22.py   800 200 100
+python fernandez_Gastelo/multiple22.py   200 100 150
+python fernandez_Gastelo/multiple22.py   80  100 20
+
+python fernandez_Gastelo/multiple23.py   900 100
+python fernandez_Gastelo/multiple23.py   1000 1500
+python fernandez_Gastelo/multiple23.py   200  300
+
+python fernandez_Gastelo/multiple24.py   "2"  "f"  20
+python fernandez_Gastelo/multiple24.py   "3"   "h"  30
+python fernandez_Gastelo/multiple24.py   "5"   "b"  50
+
+python fernandez_Gastelo/multiple25.py   20   5
+python fernandez_Gastelo/multiple25.py   100  2
+python fernandez_Gastelo/multiple25.py   30   4
+
+python fernandez_Gastelo/multiple26.py   "jose"   12 11 13
+python fernandez_Gastelo/multiple26.py    "rosa"  11 13 15
+python fernandez_Gastelo/multiple26.py    "sonia" 20 15 17
+
+
+python fernandez_Gastelo/multiple27.py    "veronica"  1950 2005
+python fernandez_Gastelo/multiple27.py     "sandra"   1996 2019
+python fernandez_Gastelo/multiple27.py     "andres"   1998 2020
+
+
+
+
+
+
+
+
+
+
